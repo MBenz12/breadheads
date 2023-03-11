77611b888a75308e02fbcecf4594e0d4f49ef453
mbenz12 <mbenz10120112@gmail.com>
2023-03-11 16:18:57 +0100

    structured for state

--- programs/breadheads/src/lib.rs
+++ programs/breadheads/src/lib.rs
@@ -1,12 +1,15 @@
+mod state;
+mod util;
+
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("BpBkYpEAd8vr4FynZE8g8Rrmwcr8BKS1Q8XFe7aL2tBL");
 
 #[program]
 pub mod breadheads {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
 }
